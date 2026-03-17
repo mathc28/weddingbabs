041be7d98827fd7d0a090619ddbf93ec70d60013
mathc28 <cocumathieu@gmail.com>
2026-03-17 15:36:18 +0100

    modification cloudinary

--- babylon-wedding/src/config/cloudinary.js
+++ babylon-wedding/src/config/cloudinary.js
@@ -1,4 +1,7 @@
-export const CLOUDINARY_CLOUD_NAME = 'djzewrs8z'
-export const CLOUDINARY_UPLOAD_PRESET = 'babylon_wedding'
-export const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/djzewrs8z/image/upload`
-export const CLOUDINARY_FETCH_URL = `https://res.cloudinary.com/djzewrs8z/image/upload`
+export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+export const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+export const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`
+export const CLOUDINARY_FETCH_URL = `https://res.cloudinary.com/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`
+export const CLOUDINARY_API_KEY = import.meta.env.VITE_CLOUDINARY_API_KEY
+export const CLOUDINARY_API_SECRET = import.meta.env.VITE_CLOUDINARY_API_SECRET
+export const ADMIN_PASSWORD = import.meta.env.VITE_ADMIN_PASSWORD
